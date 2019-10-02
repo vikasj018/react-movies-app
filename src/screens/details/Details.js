869e2f3775b8a3b160cab53f263e90d8b0d07387
Vikas <vikasjaiprakash.18@gmail.com>
2019-10-03 00:14:01 +0530

    check

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -6,12 +6,41 @@ import Typography from '@material-ui/core/Typography';
 import './Details.css';
 import Home from '../home/Home';
 import YouTube from 'react-youtube';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 class Details extends Component {
     constructor() {
         super();
         this.state = {
-            movie: {}
+            movie: {},
+            starIcons: [{
+                id: 1,
+                stateId: "star1",
+                color: "black"
+            },
+            {
+                id: 2,
+                stateId: "star2",
+                color: "black"
+            },
+            {
+                id: 3,
+                stateId: "star3",
+                color: "black"
+            },
+            {
+                id: 4,
+                stateId: "star4",
+                color: "black"
+            },
+            {
+                id: 5,
+                stateId: "star5",
+                color: "black"
+            }]
         }
     }
 
@@ -21,11 +50,30 @@ class Details extends Component {
             return mov.id === this.props.movieId
         })[0];
         this.setState({ currentState });
-        console.log(this.state);
     }
 
     backToHomeHandler = () => {
         ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+
+    artistClickHandler = (url) => {
+        window.location = url;
+    }
+
+    starClickHandler = (id) => {
+        let starIconList = [];
+        for (let star of this.state.starIcons) {
+            let starNode = star;
+            if (star.id <= id) {
+                starNode.color = "yellow"
+            }
+            else {
+                starNode.color = "black";
+
+            }
+            starIconList.push(starNode);
+        }
+        this.setState({ starIcons: starIconList });
     }
 
     render() {
@@ -85,7 +133,37 @@ class Details extends Component {
                     </div>
 
                     <div className="rightDetails">
+                        <Typography>
+                            <span className="bold">Rate this movie: </span>
+                        </Typography>
+                        {this.state.starIcons.map(star => (
+                            <StarBorderIcon
+                                className={star.color}
+                                key={"star" + star.id}
+                                onClick={() => this.starClickHandler(star.id)}
+                            />
+                        ))}
 
+                        <div className="bold marginBottom16 marginTop16">
+                            <Typography>
+                                <span className="bold">Artists:</span>
+                            </Typography>
+                        </div>
+                        <div className="paddingRight">
+                            <GridList cellHeight={160} cols={2}>
+                                {movie.artists != null && movie.artists.map(artist => (
+                                    <GridListTile
+                                        className="gridTile"
+                                        onClick={() => this.artistClickHandler(artist.wiki_url)}
+                                        key={artist.id}>
+                                        <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
+                                        <GridListTileBar
+                                            title={artist.first_name + " " + artist.last_name}
+                                        />
+                                    </GridListTile>
+                                ))}
+                            </GridList>
+                        </div>
                     </div>
                 </div>
             </div>
