fa2b307d84bdcef20c36f85791fc93d8de4dff6b
Vikas <vikasjaiprakash.18@gmail.com>
2019-10-03 00:32:07 +0530

    Added 'BookShow' component and Book Show button on header on details page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
@@ -11,6 +12,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow';
 
 const customStyles = {
     content: {
@@ -129,6 +131,10 @@ class Header extends Component {
         this.setState({ contact: e.target.value });
     }
 
+    bookShowHandler = (e) => {
+        ReactDOM.render(<BookShow />, document.getElementById('root'));
+    }
+
     render() {
         return (
             <div>
@@ -139,6 +145,13 @@ class Header extends Component {
                             Login
                         </Button>
                     </div>
+                    {this.props.showBookShowButton === "true" ?
+                        <div className="bookshow-button">
+                            <Button variant="contained" color="primary" onClick={this.bookShowHandler}>
+                                Book Show
+                            </Button>
+                        </div>
+                        : ""}
                 </header>
                 <Modal
                     ariaHideApp={false}
@@ -147,12 +160,11 @@ class Header extends Component {
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}
                 >
-            
                     <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />
                         <Tab label="Register" />
                     </Tabs>
-                    
+
                     {this.state.value === 0 &&
                         <TabContainer>
                             <FormControl required>
